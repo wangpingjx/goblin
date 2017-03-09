a23c49ee15eaf8acd7d0d3dbc2c5f100345839d7
wangping <wangping@iwaimai.baidu.com>
2017-03-09 16:13:32 +0800

    简单的查询构造器实现

--- db/main.go
+++ db/main.go
@@ -5,33 +5,82 @@ package db
 
 import (
     "database/sql"
-    // "log"
+    "errors"
+    "log"
 )
-// 数据库连接信息（尽量保持长连接、避免频繁open&close）
-type DB struct {
-    db *sql.DB
+
+/* 如果某个对象实现了此接口的所有方法，那么这个对象就实现了这个接口 */
+type sqlCommon interface {
+    Exec(query string, args ...interface{}) (sql.Result, error)
+    Prepare(query string) (*sql.Stmt, error)
+    Query(query string, args ...interface{}) (*sql.Rows, error)
+    QueryRow(query string, args ...interface{}) *sql.Row
 }
 
-// eg: db.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+/* 数据库连接信息（尽量保持长连接、避免频繁open&close）*/
+type DB struct {
+    db       sqlCommon       // *sql.DB
+    qb       *QueryBuilder   // 构造器
+
+    parent   *DB
+}
+
+/**
+ * eg: db.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+ */
 func Open(driver string, value string) (*DB, error) {
     var sqlDB *sql.DB
 
     sqlDB, err := sql.Open(driver, value)
-    db := &DB { db: sqlDB }
+    db         := &DB { db: sqlDB }
+    db.qb       = &QueryBuilder{ db: db }
+    db.parent   = db    // TODO 作用暂时不是非常明确
 
     return db, err
 }
 
 func (s *DB) Close() error {
-    return s.db.Close()
+    if db, ok := s.parent.db.(*sql.DB); ok {
+        return db.Close()
+    }
+    return errors.New("can't close current db")
 }
 
-// 获取数据库连接
+/* 返回 DB 中的 *sql.DB */
 func (s *DB) DB() *sql.DB {
-	return s.db
+	return s.db.(*sql.DB)
 }
 
-func (s *DB) Query(sql string) (rows *sql.Rows, err error) {
-    rows, err = s.db.Query(sql)
-    return rows, err
+/* 直接裸SQL查询 */
+func (s *DB) Query(query string, args ...interface{}) (*sql.Rows, error) {
+    return s.DB().Query(query, args)
 }
+
+/************************
+ *  用于组建查询的基本方法  *
+ ************************/
+func (s *DB) Table(name string) *DB {
+    return s.qb.Table(name).db
+}
+
+func (s *DB) Select(selects string) *DB {
+    return s.qb.Select(selects).db
+}
+
+func (s *DB) Where(query interface{}, args ...interface{}) *DB {
+    return s.qb.Where(query, args...).db
+}
+
+func (s *DB) Limit(limit int) *DB {
+    return s.qb.Limit(limit).db
+}
+
+func (s *DB) Find() (*sql.Rows, error) {
+    sql := s.qb.buildSelect()
+    log.Println("=> sql: " + sql)
+    return s.DB().Query(sql)
+}
+
+/* private method */
+/* TODO 为了了解clone的作用，暂不实现它，看在什么时候会遇到坑 */
+// func (s *DB) clone() *DB {}
--- db/query_builder.go
+++ db/query_builder.go
@@ -1,30 +1,22 @@
 package db
 
 import (
-    "log"
+    // "log"
+    "fmt"
+    "strings"
+    "reflect"
+    "strconv"
 )
 
 type QueryBuilder struct {
-    tableName   string
+    db           *DB
+    tableName    string
+    operation    string
 
-    whereConds  []map[string]interface{}
-    selects     map[string]interface{}
-    limit       interface{}
-}
+    whereConditions   []map[string]interface{}
 
-func (qb *QueryBuilder) Where(query interface{}, args ...interface{}) *QueryBuilder {
-    qb.whereConds = append(qb.whereConds, map[string]interface{}{"query": query, "args": args})
-    return qb
-}
-
-func (qb *QueryBuilder) Select(query interface{}, args ...interface{}) *QueryBuilder {
-    qb.selects = map[string]interface{}{"query": query, "args":args }
-    return qb
-}
-
-func (qb *QueryBuilder) Limit(limit interface{}) *QueryBuilder {
-    qb.limit = limit
-    return qb
+    selects      string
+    limit        int
 }
 
 func (qb *QueryBuilder) Table(name string) *QueryBuilder {
@@ -32,8 +24,72 @@ func (qb *QueryBuilder) Table(name string) *QueryBuilder {
     return qb
 }
 
+func (qb *QueryBuilder) Where(query interface{}, args ...interface{}) *QueryBuilder {
+    qb.whereConditions = append(qb.whereConditions, map[string]interface{}{"query": query, "args": args})
+    return qb
+}
+
+func (qb *QueryBuilder) Select(selects string) *QueryBuilder {
+    qb.operation = "SELECT"
+    qb.selects   = selects
+    return qb
+}
+
+func (qb *QueryBuilder) Limit(limit int) *QueryBuilder {
+    qb.limit = limit
+    return qb
+}
+
+/* TODO 暂时默认 query 只会是string，以后再扩充功能 */
+/* TODO 类型断言太恶心了，要改 */
+func (qb *QueryBuilder) buildWhereCondition(cond map[string]interface{}) (sql string) {
+    sql = fmt.Sprintf("(%v)", cond["query"])
+
+    args := cond["args"].([]interface{})
+    for _, arg := range args {
+        switch reflect.ValueOf(arg).Kind() {
+        // 数组 Eg: where("id in (?)", []int(1,2))
+        case reflect.Slice:
+            values := reflect.ValueOf(arg)
+            var tmps []string
+            for i := 0; i < values.Len(); i++ {
+                tmp := values.Index(i).Interface()
+                if tmp, ok := tmp.(string); ok {
+                    tmps = append(tmps, "'" + tmp + "'")
+                }
+                if tmp, ok := tmp.(int); ok {
+                    tmps = append(tmps, strconv.Itoa(tmp))
+                }
+            }
+            sql = strings.Replace(sql, "?", strings.Join(tmps, ","), 1)
+        default:
+            arg = reflect.ValueOf(arg).Interface()
+            if arg, ok := arg.(string); ok {
+                sql = strings.Replace(sql, "?", "'" + arg + "'", 1)
+            }
+            if arg, ok := arg.(int); ok {
+                sql = strings.Replace(sql, "?", strconv.Itoa(arg), 1)
+            }
+        }
+    }
+    return sql
+}
+
+func (qb *QueryBuilder) buildWhereSQL() (string) {
+    var whereConditions []string
+    for _, cond := range qb.whereConditions {
+        if sql := qb.buildWhereCondition(cond); sql != "" {
+            whereConditions = append(whereConditions, sql)
+        }
+    }
+    return strings.Join(whereConditions, " AND ")
+}
+
+func (qb *QueryBuilder) buildSelect() (sql string) {
+    sql = fmt.Sprintf("SELECT %v FROM %v WHERE %v", qb.selects, qb.tableName, qb.buildWhereSQL())
+    return sql
+}
+
 func (qb *QueryBuilder) ToString() string {
-    log.Println("=> tableName: " + qb.tableName)
-    log.Printf("=> WhereConds: %v", qb.whereConds)
-    return "I'm a sql string"
+    return qb.buildSelect())
 }
